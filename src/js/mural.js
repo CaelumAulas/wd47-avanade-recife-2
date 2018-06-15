74a2d0c2c40ea1b64c840c590e4ea466faab2627
Artur Diniz Adam <artur.daz@gmail.com>
2018-06-15 11:35:57 -0300

    Acabou o Ceep

--- src/js/mural.js
+++ src/js/mural.js
@@ -32,39 +32,40 @@ const adicionarCartao = (function(){
     // Exportando um resultado
     // Público
     // Closure, Fechamento, Clausura
-    // Exportar 
+    // adicionarCartao
     return function (conteudo, cor = "#EBEF40"){
-
-        listaCartoes.push({
+        const cartao = {
             conteudo:  conteudo,
             cor: cor
-        })
+        }
+
+        listaCartoes.push(cartao)
         
         numeroCartao = numeroCartao + 1
     
-        const cartao = $(`
+        const $cartao = $(`
         <article id="cartao_${numeroCartao}" class="cartao " tabindex="0" style="background-color: ${cor}">
             <div class="opcoesDoCartao">
             <button class="opcoesDoCartao-remove opcoesDoCartao-opcao" tabindex="0">
                 <svg><use xlink:href="#iconeRemover"></use></svg>
             </button>
     
-            <input type="radio" name="corDoCartao${numeroCartao}" value="${cor}" id="corPadrão-cartao${numeroCartao}" class="opcoesDoCartao-radioTipo" checked>
-            <label for="corPadrão-cartao${numeroCartao}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: ${cor};" tabindex="0">
+            <input type="radio" name="corDoCartao${numeroCartao}" value="#EBEF40" id="corPadrão-cartao${numeroCartao}" class="opcoesDoCartao-radioTipo" ${cor == '#EBEF40' ? 'checked' : '' }>
+            <label for="corPadrão-cartao${numeroCartao}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #EBEF40;" tabindex="0">
                 Padrão
             </label>
     
-            <input type="radio" name="corDoCartao${numeroCartao}" value="#F05450" id="corImportante-cartao${numeroCartao}" class="opcoesDoCartao-radioTipo">
+            <input type="radio" name="corDoCartao${numeroCartao}" value="#F05450" id="corImportante-cartao${numeroCartao}" class="opcoesDoCartao-radioTipo" ${cor == '#F05450' ? 'checked' : '' }>
             <label for="corImportante-cartao${numeroCartao}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #F05450;" tabindex="0">
                 Importante
             </label>
     
-            <input type="radio" name="corDoCartao${numeroCartao}" value="#92C4EC" id="corTarefa-cartao${numeroCartao}" class="opcoesDoCartao-radioTipo">
+            <input type="radio" name="corDoCartao${numeroCartao}" value="#92C4EC" id="corTarefa-cartao${numeroCartao}" class="opcoesDoCartao-radioTipo" ${cor == '#92C4EC' ? 'checked' : '' }>
             <label for="corTarefa-cartao${numeroCartao}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #92C4EC;" tabindex="0">
                 Tarefa
             </label>
     
-            <input type="radio" name="corDoCartao${numeroCartao}" value="#76EF40" id="corInspiração-cartao${numeroCartao}" class="opcoesDoCartao-radioTipo">
+            <input type="radio" name="corDoCartao${numeroCartao}" value="#76EF40" id="corInspiração-cartao${numeroCartao}" class="opcoesDoCartao-radioTipo" ${cor == '#76EF40' ? 'checked' : '' }>
             <label for="corInspiração-cartao${numeroCartao}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #76EF40;" tabindex="0">
                 Inspiração
             </label>
@@ -73,23 +74,35 @@ const adicionarCartao = (function(){
         </article>
         `)
     
-        cartao
+        $cartao
             .on("focusin", function(){
-                cartao.addClass("cartao--focado")
+                $cartao.addClass("cartao--focado")
             })
             .on("focusout", function(){
-                cartao.removeClass("cartao--focado")
+                $cartao.removeClass("cartao--focado")
             })
             .on("click", ".opcoesDoCartao-remove" ,function(evento){
-                cartao.addClass("cartao--some")
-                cartao.on("transitionend", function remove(){
-                    cartao.remove()
+                $cartao.addClass("cartao--some")
+                $cartao.on("transitionend", function remove(){
+                    $cartao.remove()
+
+                    const posicaoDoCartao = listaCartoes.indexOf(cartao)
+                    listaCartoes.splice(posicaoDoCartao, 1)
                 })
             })
             .on("change", ".opcoesDoCartao-radioTipo", function(evento){
                 const inputSelecionado = evento.target
                 const cor = inputSelecionado.value
-                cartao.css("background-color", cor)
+                
+                $cartao.css("background-color", cor)
+                
+                cartao.cor = cor
+                
+            })
+            .on("input", ".cartao-conteudo", function(evento){
+                const $p = evento.target
+
+                cartao.conteudo = $p.textContent
             })
             .on("keydown", ".opcoesDoCartao-tipo", function(evento){
                 console.log(evento.key)
@@ -98,7 +111,7 @@ const adicionarCartao = (function(){
                 }
             })  
 
-        cartao.appendTo('.mural')
+        $cartao.appendTo('.mural')
     
     }
 })()
--- src/js/opcoesDaPagina/btnSync.js
+++ src/js/opcoesDaPagina/btnSync.js
@@ -5,8 +5,9 @@
 
     $btnSync.on("click", function(){
 
+        $btnSync.removeClass("botaoSync--sincronizado")
+        $btnSync.removeClass("botaoSync--deuRuim")
         $btnSync.addClass("botaoSync--esperando")
-
 
         $.ajax({
             url: "http://ceep.herokuapp.com/cartoes/salvar",
@@ -18,13 +19,13 @@
             success: function(){
                 $btnSync.addClass("botaoSync--sincronizado")
                 $btnSync.removeClass("botaoSync--esperando")
+            },
+            error: function(){
+                $btnSync.addClass("botaoSync--deuRuim")
+                $btnSync.removeClass("botaoSync--esperando")
             }
         })
-
-
     })
-
-
 
     $btnSync.removeClass("no-js")
 
